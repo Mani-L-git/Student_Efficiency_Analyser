99dbb6cf5d6c567500c83b76ed863929cd3ff7dc
Manikandan <manivpel24@gmail.com>
2026-03-25 17:56:09 +0530

    Dp

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+   port:     process.env.DB_PORT || 3306,  
 });
 
 /* ==========================
